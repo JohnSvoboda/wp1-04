3ceb7b87c98c858f323b50b1c395dc2ea2f25b45
Vaše Jméno <vas@email.com>
2018-03-22 15:21:53 +0100

    podmínka

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>proměnné</h1>
+    <?php
+    $age= 5 ;
+
+     ?>
+     <?php if ($age>=16) {  ?>
+     <img src="img/BCH.jpg" alt="Becherovka"> ;
+
+     <?php } else {echo "sice máš občanku, ale ještě nemůžeš pít"; ?>
+     <img src="img/RB.jpg" alt="Robby bubble"> ;
+   <?php  }
+
+ ?>
+
+  </body>
+</html>
